2235310fe87377d422f8fb13b8a5a3464553eb28
gscarramberg <gscarramberg@getitsmart.net>
2018-10-18 17:45:01 -0300

    dalee

--- src/app/main-screen/news-grid/news-grid.component.ts
+++ src/app/main-screen/news-grid/news-grid.component.ts
@@ -15,21 +15,11 @@ import { trigger, state, transition, style, animate } from '../../../../node_mod
   templateUrl: './news-grid.component.html',
   styleUrls: ['./news-grid.component.css'],
   animations: [
-    // the fade-in/fade-out animation.
-    trigger('simpleFadeAnimation', [
-
-      // the "in" style determines the "resting" state of the element when it is visible.
-      state('in', style({opacity: 1})),
-
-      // fade in when created. this could also be written as transition('void => *')
-      transition(':enter', [
-        style({opacity: 0}),
-        animate(500 )
-      ]),
-
-      // fade out when destroyed. this could also be written as transition('void => *')
-      transition(':leave',
-        animate(600, style({opacity: 0})))
+    trigger('Fading', [
+      state('void', style({ opacity: 0 })),
+      state('*', style({ opacity: 1 })),
+      transition(':enter', animate('500ms ease-out')),
+      transition(':leave', animate('500ms ease-in')),
     ])
   ]
 })
